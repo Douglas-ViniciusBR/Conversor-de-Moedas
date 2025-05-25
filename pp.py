32cc2adc9e683418e0a9269d4deb8f034748d9a6
Douglas Vinícius <vdouglas893@gmail.com>
2025-05-25 16:29:16 -0300

    Commit Inicial

--- pp.py
+++ pp.py
@@ -8,12 +8,12 @@ taxas = {
 } 
             
 moeda_quantia = float(input("Qual é o valor que deseja converter?:"))
-moeda_origem = input("Qual é a origem da moeda? EX: BRL, USD, EUR e etc..):") .upper() 
-converter = input("Deseja converter pra qual moeda?") .upper()
+moeda_origem = input("Qual é a origem da moeda? EX: BRL, USD, EUR e etc..):")
+converter = input("Deseja converter pra qual moeda?")
 
 if moeda_origem in taxas and converter in taxas:
     valor_Real = moeda_quantia * taxas[moeda_origem] 
     valorFinal = valor_Real / taxas[converter]
     print("O valor convertido da", moeda_origem ,":", moeda_quantia , "É" , round(valorFinal, 2), converter)
 else : 
-    print("Moeda inválida. Porque favor, tente novamente.")
+    print("Moeda inválida. Porque favor, tente novamemnte.")
