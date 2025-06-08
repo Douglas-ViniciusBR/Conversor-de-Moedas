81cbf168ff54c90397050840061c72b6a9833ed7
BrunoSena7 <senabrunomessi@gmail.com>
2025-06-08 13:26:08 -0300

    Adiciona arquivo conversor.py

---
+++ pp.py
@@ -0,0 +1,19 @@
+taxas = {
+
+    "BRL" : 1.00,
+    "USD" : 5.61,
+    "EUR" : 6.29,
+    "GBP" : 7.43,
+    "JPY" : 0.038,
+} 
+            
+moeda_quantia = float(input("Qual é o valor que deseja converter?:"))
+moeda_origem = input("Qual é a origem da moeda? EX: BRL, USD, EUR e etc..):")
+converter = input("Deseja converter pra qual moeda?")
+
+if moeda_origem in taxas and converter in taxas:
+    valor_Real = moeda_quantia * taxas[moeda_origem] 
+    valorFinal = valor_Real / taxas[converter]
+    print("O valor convertido da", moeda_origem ,":", moeda_quantia , "É" , round(valorFinal, 2), converter)
+else : 
+    print("Moeda inválida. Porque favor, tente novamemnte.")
